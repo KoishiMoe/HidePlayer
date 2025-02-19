72309a584b8585e026076f3df6d7c0d8f5fe68b0
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2025-02-19 09:06:07 +0800

    fix client uuid leakage

---
+++ src/main/java/dev/lolihub/hideplayer/mixin/ServerChunkLoadingManagerMixin.java
@@ -0,0 +1,31 @@
+package dev.lolihub.hideplayer.mixin;
+
+import dev.lolihub.hideplayer.HidePlayer;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(targets = "net.minecraft.server.world.ServerChunkLoadingManager$EntityTracker")
+public class ServerChunkLoadingManagerMixin {
+    @Final
+    @Shadow
+    Entity entity;
+
+    @Inject(
+            at = @At("HEAD"),
+            method = "updateTrackedStatus(Lnet/minecraft/server/network/ServerPlayerEntity;)V",
+            cancellable = true
+    )
+    // Prevents server from sending entity spawn packet to players who shouldn't see the player. This solves the issue of leaking player's UUID.
+    private void onUpdateTrackedStatus(ServerPlayerEntity player, CallbackInfo ci) {
+        if (entity instanceof ServerPlayerEntity && player != entity &&
+                !HidePlayer.getVisibilityManager().getPlayerCapability((ServerPlayerEntity) entity).showInGame(player)) {
+            ci.cancel();
+        }
+    }
+}
