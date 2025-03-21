KoishiMoe/HidePlayer

commit a2136f6ce2384ca373bbe9c65f9bea9dc0f4b86b

fix a death msg leak
wtf

src/main/java/dev/lolihub/hideplayer/mixin/DamageTrackerMixin.java
@@ -29,13 +29,6 @@ public class DamageTrackerMixin {
     // death message
     @Inject(method = "getDeathMessage", at = @At("RETURN"), cancellable = true)
     private void onGetDeathMessage(CallbackInfoReturnable<Text> cir) {
-        // death of hidden player
-        if (entity instanceof ServerPlayerEntity player) {
-            if (HidePlayer.getVisibilityManager().getPlayerCapability(player).hideSystemMessage()) {
-                cir.setReturnValue(new HiddenPlayerText(cir.getReturnValue(), player));
-            }
-        }
-
         // death caused by hidden player
         if (recentDamage.isEmpty()) return;
         DamageSource source = recentDamage.getLast().damageSource();
@@ -49,6 +42,14 @@ public class DamageTrackerMixin {
         }
         if (HidePlayer.getVisibilityManager().getPlayerCapability(player).hideSystemMessage()) {
             cir.setReturnValue(new HiddenPlayerKillText(cir.getReturnValue(), entity, player));
+        }
+
+        // death of hidden player
+        // check this after the above check to avoid hidden player killing hidden player causing leak
+        if (entity instanceof ServerPlayerEntity player2) {
+            if (HidePlayer.getVisibilityManager().getPlayerCapability(player2).hideSystemMessage()) {
+                cir.setReturnValue(new HiddenPlayerText(cir.getReturnValue(), player2));
+            }
         }
     }
 }

commit 9ebe49083672226a09d35584658a40117f578db9

fix death message leak caused by a2136f6ce2384ca373bbe9c65f9bea9dc0f4b86b

src/main/java/dev/lolihub/hideplayer/mixin/DamageTrackerMixin.java
@@ -30,18 +30,17 @@ public class DamageTrackerMixin {
     @Inject(method = "getDeathMessage", at = @At("RETURN"), cancellable = true)
     private void onGetDeathMessage(CallbackInfoReturnable<Text> cir) {
         // death caused by hidden player
-        if (recentDamage.isEmpty()) return;
-        DamageSource source = recentDamage.getLast().damageSource();
-        ServerPlayerEntity player;
-        if (source.getSource() instanceof ServerPlayerEntity) {
-            player = (ServerPlayerEntity) source.getSource();
-        } else if (entity.getPrimeAdversary() instanceof ServerPlayerEntity) {
-            player = (ServerPlayerEntity) entity.getPrimeAdversary();
-        } else {
-            return;
-        }
-        if (HidePlayer.getVisibilityManager().getPlayerCapability(player).hideSystemMessage()) {
-            cir.setReturnValue(new HiddenPlayerKillText(cir.getReturnValue(), entity, player));
+        if (!recentDamage.isEmpty()) {
+            DamageSource source = recentDamage.getLast().damageSource();
+            ServerPlayerEntity player = null;
+            if (source.getSource() instanceof ServerPlayerEntity) {
+                player = (ServerPlayerEntity) source.getSource();
+            } else if (entity.getPrimeAdversary() instanceof ServerPlayerEntity) {
+                player = (ServerPlayerEntity) entity.getPrimeAdversary();
+            }
+            if (player != null && HidePlayer.getVisibilityManager().getPlayerCapability(player).hideSystemMessage()) {
+                cir.setReturnValue(new HiddenPlayerKillText(cir.getReturnValue(), entity, player));
+            }
         }
 
         // death of hidden player